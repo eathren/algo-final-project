e535be1320204b37ca4d66a20e6b60a561ece268
eathren <nolan.braman@gmail.com>
2022-08-16 15:22:01 -0500

    possible fix for vercel failed build

--- components/GraphContainer/GraphContainer.tsx
+++ components/GraphContainer/GraphContainer.tsx
@@ -4,11 +4,7 @@ import { Config, names, uniqueNamesGenerator } from "unique-names-generator";
 import { v4 as uuidv4 } from "uuid";
 import { Rider, Taxi, Variant } from "../../types";
 
-import {
-  BackendFormatGraph,
-  createTaxiGraph,
-  TaxiGraph,
-} from "../../utils/index";
+import { BackendFormatGraph, createTaxiGraph, TaxiGraph } from "../../utils/";
 
 import RiderList from "../RiderList";
 
