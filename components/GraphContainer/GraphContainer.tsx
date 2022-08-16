9be082e8118ac0ad846d9d1e335beb460889bb9d
eathren <nolan.braman@gmail.com>
2022-08-16 15:27:00 -0500

    possible fix for vercel failed build

--- components/GraphContainer/GraphContainer.tsx
+++ components/GraphContainer/GraphContainer.tsx
@@ -3,7 +3,11 @@ import CytoscapeComponent from "react-cytoscapejs";
 import { Config, names, uniqueNamesGenerator } from "unique-names-generator";
 import { v4 as uuidv4 } from "uuid";
 import { Rider, Taxi, Variant } from "../../types";
-import { BackendFormatGraph, createTaxiGraph, TaxiGraph } from "../../utils";
+import {
+  BackendFormatGraph,
+  createTaxiGraph,
+  TaxiGraph,
+} from "../../utils/utils";
 
 import RiderList from "../RiderList";
 
